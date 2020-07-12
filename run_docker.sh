ed90e05f57965336441b467ba5e6aea5fbcae916
Waqas <wakassadik@gmail.com>
2020-07-12 18:41:35 +0500

    to push the image to github, had to add mydockerId/image_name for uploading

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build . --tag project4_attempt_1
+docker build . --tag qasibeat/project4_attempt_1
 # Step 2: 
 # List docker images
 docker image ls
