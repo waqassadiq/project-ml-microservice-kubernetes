71e1bb3eeed8c86ea8e43fb50ac34718337a29ff
Waqas <wakassadik@gmail.com>
2020-07-12 13:06:08 +0500

    completing the run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag project4_attempt_1
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+sudo docker run --name project4_attempt_1 -p 5001:5001 project4_attempt_1
