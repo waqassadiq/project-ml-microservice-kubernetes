fc990ad6ba5d46d1fdf5a9c5d21902efb27865d0
Waqas <wakassadik@gmail.com>
2020-07-13 13:05:59 +0500

    prediction is working with kuberneties

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=qasibeat/project4attempt2
+dockerpath=docker.io/qasibeat/project4attempt2
 
 # Step 2
 # Run the Docker Hub container with kubernetes
@@ -18,3 +18,8 @@ kubectl get pods
 # Step 4:
 # Forward the container port to a host
 kubectl expose deployment project4attempt2 --type=LoadBalancer --port=8000 --target-port=80
+
+# Step 5: by waqas
+# adding port forward to the the prediction running and get access to it
+# after step 4, run thee following command manually
+# kubectl port-forward project4attempt2-56f6f56648-xxt7b  8000:80
