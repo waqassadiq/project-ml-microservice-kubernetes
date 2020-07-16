0af209dc43a1adc1f8c65af29b320c83e9c4b7ca
Waqas <wakassadik@gmail.com>
2020-07-16 21:42:19 +0500

    everything is working fine now till this point

--- app.py
+++ app.py
@@ -71,3 +71,4 @@ if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+## uptill now everthing is working fine
